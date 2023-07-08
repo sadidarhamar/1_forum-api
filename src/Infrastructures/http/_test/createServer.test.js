dd54aab4404d72a91ef5e30f8865298f28d86c5e
irham <sadid.dev@gmail.com>
2023-07-08 09:08:53 +0700

    add say hello world test case

--- src/Infrastructures/http/_test/createServer.test.js
+++ src/Infrastructures/http/_test/createServer.test.js
@@ -1,34 +1,50 @@
-const createServer = require('../createServer');
+const createServer = require("../createServer");
 
-describe('HTTP server', () => {
-  it('should response 404 when request unregistered route', async () => {
+describe("HTTP server", () => {
+  it("should response 404 when request unregistered route", async () => {
     const server = await createServer({});
 
     const response = await server.inject({
-      method: 'GET',
-      url: '/unregisteredRoute',
+      method: "GET",
+      url: "/unregisteredRoute",
     });
 
     expect(response.statusCode).toEqual(404);
   });
 
-  it('should handle server error correctly', async () => {
+  it("should handle server error correctly", async () => {
     const requestPayload = {
-      username: 'dicoding',
-      fullname: 'Dicoding Indonesia',
-      password: 'super_secret',
+      username: "dicoding",
+      fullname: "Dicoding Indonesia",
+      password: "super_secret",
     };
     const server = await createServer({});
 
     const response = await server.inject({
-      method: 'POST',
-      url: '/users',
+      method: "POST",
+      url: "/users",
       payload: requestPayload,
     });
 
     const responseJson = JSON.parse(response.payload);
     expect(response.statusCode).toEqual(500);
-    expect(responseJson.status).toEqual('error');
-    expect(responseJson.message).toEqual('terjadi kegagalan pada server kami');
+    expect(responseJson.status).toEqual("error");
+    expect(responseJson.message).toEqual("terjadi kegagalan pada server kami");
+  });
+
+  describe("when GET /", () => {
+    it("should return 200 and hello world", async () => {
+      // Arrange
+      const server = await createServer({});
+      // Action
+      const response = await server.inject({
+        method: "GET",
+        url: "/",
+      });
+      // Assert
+      const responseJson = JSON.parse(response.payload);
+      expect(response.statusCode).toEqual(200);
+      expect(responseJson.value).toEqual("Hello world!");
+    });
   });
 });
